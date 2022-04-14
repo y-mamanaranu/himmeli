94f318d112f164ab0cfa6eb39ba4ce95ad4b6e2a
Yoiduki <90896806+y-muen@users.noreply.github.com>
2022-04-14 23:11:55 +0900

    Delete Himmeli directory

--- Himmeli/__init__.py
+++
@@ -1,7 +0,0 @@
-"Himmeli"
-__version__ = '1.0.0'
-# from .model.bicone import Bicone
-# from .model.bicone_lack import Bicone_Lack
-# from .model.cone import Cone
-# from .model.icosahedron import Icosahedron
-# from .model.star import Star
